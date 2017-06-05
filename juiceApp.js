a58fe665e51aea991d28966d50e776ec87d000e7
Chris Barriteau <chris4finance@gmail.com>
2017-06-05 03:36:02 -0400

    made revisions to codebase. This is the final commit

--- juiceApp.js
+++ juiceApp.js
@@ -1,34 +1,31 @@
+// Part 1 ------------------------------------->
 function Food(name) {
 	this.name = name;
 
 	}
 
-}
-
 Food.prototype.juicer = function() {
 	console.log('Measure and add ' + this.name + " to the juicer" );
-}
+};
 
 // code below creates Fruit object that inherits from Food
-
 function Fruit () {
 	Food.apply(this,arguments);
 }
 
 Fruit.prototype = new Food();
 
-// Code below creates a new instance of fruit
 
+// Code below creates a new instance of fruit
 var berry = new Fruit('Strawberry');
 
-// Code below adds a new method, sayName()
 
+// Code below adds a new method, sayName()
 Fruit.prototype.sayName = function() {
 	console.log('This fruit is called ' + this.name);
-}
+};
 
 // Code defines an apple contructor that takes from Fruit
-
 var apple = new Fruit('');
 apple.name = ['Golden Delicious', 'Red Delicious','Gala','Macintosh','Granny Smith'];
 
@@ -36,9 +33,8 @@ apple.name = ['Golden Delicious', 'Red Delicious','Gala','Macintosh','Granny Smi
 apple.hasSkin = true;
 
 
-// code below creates a removeSkin method on Fruit constructor
+// code below creates a removeSkin() method on Fruit constructor
 // also prints message to the console.
-
 Fruit.prototype.removeSkin = function(){
   
   if(this.hasSkin){
@@ -54,6 +50,7 @@ Fruit.prototype.removeSkin = function(){
          item += nameArr[i] + ", " ;
     }
       console.log('Apples cannot be juiced until their skin is removed. Therefore skin is being removed.' + '\nVariety of these fruits to juice are' + item + '.');
+      
   } else {
     
     console.log('Apple cannot be juiced until it\'s skin is removed. Therefore skin is being removed.' + '\nThis type ' + item + ', is a good fruit to juice');
@@ -64,8 +61,7 @@ console.log(apple.removeSkin());
 
 
 // code below creates a Vegetable object that inherits from Food... and creates a new 
-// instance of carrot from Vegetable object
-
+// instance of carrot from Vegetable object... it includes logic to parse nested array
 function Vegetable() {
   this.wash = function() {
       console.log('vegetables are being washed');
@@ -75,27 +71,32 @@ function Vegetable() {
 
 Vegetable.prototype = new Food();
 
-var carrot = new Vegetable();
+var carrot = new Vegetable('carrot');
 
 
-// Code below is Part II of Assignment
+// Code below is Part II of Assignment ---------------->
 
 //Code below creates an array of two categories of Apples. Popular and unPopular apples;
-
 var popApples = ['Golden Delicious', 'Red Delicious','Gala','Granny Smith','Empire'];
-var unPopApples = ['Macintosh', 'Washington', 'Braeburn', 'Gravenstein']
+var unPopApples = ['Macintosh', 'Washington', 'Braeburn', 'Gravenstein'];
 
 var favApples = new Fruit(popApples);
 var nonFavApples = new Fruit(unPopApples);
 
 
 //Code below says name of each Apple in Array
-console.log(favApples.name)
-console.log(nonFavApples.name)
+console.log(favApples.name);
+console.log(nonFavApples.name);
 
 
-// code below runs juice method on each category of apples
+// code below runs juice() method on each category of apples
+console.log(favApples.juicer());
+console.log(nonFavApples.juicer());
 
-console.log(popularApples.juicer());
-console.log(unpopularApples.juicer());
+
+// code below runs wash() and then juicer method for instance of carrot on Vegetable object
+console.log(carrot.wash());
+console.log(carrot.juicer());
+
+
 
